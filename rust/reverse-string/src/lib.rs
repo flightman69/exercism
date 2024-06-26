8bf000440143aa6442e960c2cc0d2cbf6df17fb5
flightman69 <flightman69420@gmail.com>
2024-06-26 23:22:58 +0530

    Gigasecond

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,17 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let mut string = String::new();
-
-    let graphemes: Vec<&str> = input.graphemes(true).collect();
-
-    for i in (0..input.len()).rev() {
-        if let Some(ch) = graphemes.get(i) {
-            string.push_str(ch);
-        } else {
-            println!("Failed to reverse a string");
-        }
-    }
-
-    string
+    input.graphemes(true).rev().collect()
 }
